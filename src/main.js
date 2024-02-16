3a4db16a9989829af7125fd3d116b757452351c8
Ton <146097352+wiangin@users.noreply.github.com>
2024-02-16 11:23:25 +0100

    ready for deployment

--- src/main.js
+++ src/main.js
@@ -1,4 +1,11 @@
 import imageUrl from "./image/enter.png"
+import anime from "animejs";
 
 const image = document.querySelector('img');
 image.src = imageUrl;
+
+anime({
+    targets:image,
+    rotate: 360
+
+})
